d28b5ff464b0ddd5ef874dd43b155d0e3a3a1a08
Cheruiyotd <korirkipnisden@gmail.com>
2020-09-09 04:56:02 +0300

    ApiUtil.getJson removed

--- app/src/main/java/com/mcheru/leaderboard/ApiUtil.java
+++ app/src/main/java/com/mcheru/leaderboard/ApiUtil.java
@@ -26,33 +26,6 @@ public class ApiUtil {
         }
     }
 
-    public static String getJson(URL url) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        try {
-            InputStream stream = connection.getInputStream();
-            Scanner scanner= new Scanner(stream);
-            scanner.useDelimiter("\\A");
-            boolean hasData = scanner.hasNext();
-
-            if (hasData){
-                return scanner.next();
-            }
-            else {
-                return null;
-            }
-
-        }
-        catch (Exception e) {
-            Log.d("Error", e.toString());
-            e.printStackTrace();
-            return null;
-        }
-        finally {
-            connection.disconnect();
-        }
-    }
-
     public static String getFullUrlAsString(String s) {
         return BASE_API_URL+s;
     }
